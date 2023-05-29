fe1295dfebdfac1ce4baaa5345d601b6b233301c
Lorenzo26J <derkshoith13@gmail.com>
2023-05-29 09:19:20 -0500

    Rutina_Recreativa CRUD

---
+++ src/main/java/pe/edu/pe/benunathani/disponibilidad/dtos/Rutina_RecreativaDTO.java
@@ -0,0 +1,32 @@
+package pe.edu.pe.benunathani.disponibilidad.dtos;
+
+
+public class Rutina_RecreativaDTO {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
